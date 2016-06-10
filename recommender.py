76b4303ddda251cfaff790341870bed7a56c178e
Colton Stapper <colton@vargas.com>
2016-06-10 06:06:56 -0700

    speed up recommender and make it more accurate.

--- recommender.py
+++ recommender.py
@@ -7,8 +7,8 @@ def cosineSimilarity(ratings_table, users, movie1, movie2):
     denom2 = 0.
     for u in users:
     	if (u, movie1) in ratings_table and (u, movie2) in ratings_table:
-    		r1 = ratings_table[u, movie1]
-    		r2 = ratings_table[u, movie2]
+    		r1 = float(ratings_table[u, movie1])
+    		r2 = float(ratings_table[u, movie2])
 	        numerator += r1 * r2
 	        denom1 += r1**2.0
 	        denom2 += r2**2.0
@@ -24,28 +24,26 @@ def itemPrediction(ratings_table, user_id, movie_id, users, movies):
         if m != movie_id:
         	cos_sim = cosineSimilarity(ratings_table, users, movie_id, m)
         	if (user_id, m) in ratings_table:
-        		s += cos_sim * ratings_table[user_id, m]
-        	else:
-        		s += cos_sim	
-        		k += abs(cos_sim)
+        		s += cos_sim * float(ratings_table[user_id, m])
+        	k += abs(cos_sim)
     if k == 0:
     	return 0.
-    k = 1.0 / k
-    return k * s
+    return s / k
 
 def recommendMovies(ratings_table, user_id, users, movies, N=4):
 	i = 0
 	predicts = {}
 	recommended = []
 	for m in movies:
-		if i%10 == 0:
-			prediction = itemPrediction(ratings_table, user_id, m, users, movies)
-			predicts[m] = prediction
-		i+=1
+		if (user_id, m) not in ratings_table:
+			if i%20 == 0:
+				prediction = itemPrediction(ratings_table, user_id, m, users, movies)
+				print(prediction)
+				predicts[m] = prediction
+			i+=1
 	sort = sorted(predicts.items(), key=operator.itemgetter(1), reverse=True)[:N]
 	for s in sort:
 		recommended.append(s[0])
-	# print(recommended)
 	return recommended
 
 
